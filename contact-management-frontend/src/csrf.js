4636c147a4860ffa9a0aef569cceaed26034081c
Aalian Taqi <aaliantaqi@gmail.com>
2026-08-21 08:15:12 +0500

    Remove frontend files

--- contact-management-frontend/src/csrf.js
+++
@@ -1,6 +0,0 @@
-export function getCsrfToken() {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; XSRF-TOKEN=`);
-    if (parts.length === 2) return parts.pop().split(';').shift();
-    return null;
-}
